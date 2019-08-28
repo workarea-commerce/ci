9b166d606ab04eec58bc68aa830b0b5fd2500945
Ben Crouse <bcrouse@workarea.com>
2019-08-28 16:03:44 -0400

    Add bundler-audit

---
+++ bundler-audit/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Exit if any subcommand fails
+set -e
+cd $GITHUB_WORKSPACE
+
+if [ -f "Gemfile" ]; then
+  echo "# Bundling..."
+  bundle install --jobs 4 --retry 3
+fi
+
+# Setup bundler-audit if needed
+if [ ! `which bundler-audit` ]; then
+  echo "\n# Installing bundler-audit..."
+  gem install bundler-audit
+fi
+
+echo "\n# Running bundler-audit..."
+sh -c "bundle audit check --update"
