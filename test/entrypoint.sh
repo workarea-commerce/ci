21e520b6731647c4943a287f1d5b219cf5c77df0
Ben Crouse <bcrouse@workarea.com>
2019-08-28 17:03:25 -0400

    Fix escaping

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 echo "# Bundling..."
 bundle install --jobs 4 --retry 3
-bundle exec $(bundle exec rake -T | grep services:up | sed 's/\\w*#.*//')
+bundle exec $(bundle exec rake -T | grep services:up | sed 's/\w*#.*//')
 
 if [ -z "$1" ]; then
   command="bin/rails test test/**/*_test.rb"
