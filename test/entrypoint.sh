abbea20e34cbb73f18b00d815c5c33f1d3b8c56a
Ben Crouse <bcrouse@workarea.com>
2019-08-29 10:23:10 -0400

    Try sleeping to allow services to come up

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -6,6 +6,7 @@ set -e
 echo "# Bundling..."
 bundle install --jobs 4 --retry 3
 bundle exec $(bundle exec rake -T | grep services:up | sed 's/\w*#.*//')
+sleep 10 # to allow services to come up
 
 if [ -z "$1" ]; then
   command="bin/rails test test/**/*_test.rb"
